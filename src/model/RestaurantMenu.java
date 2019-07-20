8be60ad7034af35b74c9534411341e2f42783c58
bourn404 <bourn404@gmail.com>
2019-07-20 11:57:29 -0600

    Created the RestaurantMenu class

--- src/model/RestaurantMenu.java
+++ src/model/RestaurantMenu.java
@@ -1,5 +1,40 @@
 package model;
 
-public class RestaurantMenu {
+import java.util.ArrayList;
 
+public class RestaurantMenu {
+	private ArrayList<RestaurantMenuItem> menuItems;
+
+	RestaurantMenu() {
+
+	}
+
+	RestaurantMenu(ArrayList<RestaurantMenuItem> menuItems) {
+		this.menuItems = menuItems;
+	}
+
+	public void addMenuItem(RestaurantMenuItem menuItem) {
+		menuItems.add(menuItem);
+	}
+
+	public void removeMenuItem(int index) {
+		menuItems.remove(index);
+	}
+
+	public RestaurantMenuItem getMenuItem(int index) {
+		return menuItems.get(index);
+	}
+
+	/**
+	 * @return the menuItems
+	 */
+	public ArrayList<RestaurantMenuItem> getMenuItems() {
+		return menuItems;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/views/Sample.java
+++ src/views/Sample.java
@@ -1,13 +1,31 @@
 package views;
+
+import model.RestaurantMenu;
+
 // import MainMenuView;
 
 public class Sample {
+	private static RestaurantMenu restaurantMenu;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MainMenuView mainMenu = new MainMenuView();
 		mainMenu.display();
 		System.out.printf("Game Over.");
+	}
+
+	/**
+	 * @return the restauraneMenu
+	 */
+	public static RestaurantMenu getRestaurantMenu() {
+		return restaurantMenu;
+	}
+
+	/**
+	 * @param restauraneMenu the restauraneMenu to set
+	 */
+	public static void setRestaurantMenu(RestaurantMenu restaurantMenu) {
+		Sample.restaurantMenu = restaurantMenu;
 	}
 
 }
